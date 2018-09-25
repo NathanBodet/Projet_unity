9ac708fc8cfa1a415cbbd055fa89fee258d2fc31
NathanBodet <bbodetnathan@gmail.com>
2018-09-25 15:59:33 +0200

    Ajout de la fonctionnalité de tir
    Il faut utiliser e pour switcher de type d'arme. le perso va tirer là où on vise avec la souris. les projectiles ne disparaissent pas encore
    
    Co-Authored-By: Bastien Tomasetti <blasterino@users.noreply.github.com>

--- ProjetTuteuré/Assets/Scripts/Player.cs
+++ ProjetTuteuré/Assets/Scripts/Player.cs
@@ -10,7 +10,8 @@ public class Player : Actor {
     Vector2 ballPos;
     public float fireRate = 0.5f;
     float nextFire = 0.0f;
-    bool typeArmeEquipee = true; // true -> arme de cac, false -> arme a distance
+    float switchCooldown = 0.0f;
+    bool typeArmeEquipee = false; // true -> arme de cac, false -> arme a distance
 
 
     protected override void Start () {
@@ -31,12 +32,18 @@ public class Player : Actor {
             animator.SetBool("IsMoving", false);
         }
 
+        if (Input.GetKey("e") && Time.time > switchCooldown) { //switch le type d'arme : cooldown de 1s
+            switchCooldown = Time.time + 1f;
+            Debug.Log("switch");
+            typeArmeEquipee = !typeArmeEquipee;
+        }
+
         if (Input.GetMouseButtonDown(0))
         {
             animator.SetTrigger("Attacking");
             if(!typeArmeEquipee && Time.time > nextFire)
             {
-                nextFire = Time.time + fireRate;
+                nextFire = Time.time + fireRate; //firerate -> cooldown de tir
                 fire();
             }
         }
@@ -44,17 +51,20 @@ public class Player : Actor {
 
     public void fire()
     {
+
         ballPos = transform.position;
-        ballPos += new Vector2(1f,0f);
-        Vector3 shootDirection;
-        shootDirection = Input.mousePosition;
-        shootDirection.z = 0.0f;
-        shootDirection = Camera.main.ScreenToWorldPoint(shootDirection);
-        shootDirection = transform.position - shootDirection;
+        ballPos += new Vector2(0.5f,0f);
 
+        //récupération des coordonnées de la souris et création du vecteur du projectile tiré
+        Vector3 ballDir = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));//10.0f car si z = 0f, la fonction retourne la position de la caméra
+        Debug.Log(ballDir);
+        ballDir.x  = ballDir.x - transform.position.x;
+        ballDir.y = ballDir.y - transform.position.y;
+        ballDir.Normalize();
 
+        //instanciation du projectile et addition du vecteur vitesse
         GameObject ballInstance = Instantiate(ball, ballPos, Quaternion.identity);
-        ballInstance.GetComponent<Rigidbody2D>().velocity = new Vector2(shootDirection.x * speed, shootDirection.y * speed);
+        ballInstance.GetComponent<Rigidbody2D>().velocity = new Vector2(ballDir.x * 5, ballDir.y * 5);
 
 
     }
