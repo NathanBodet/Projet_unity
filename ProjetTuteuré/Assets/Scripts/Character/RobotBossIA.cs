57b358d22bc50695793bddf986b3866d626c3d40
NathanBodet <bbodetnathan@gmail.com>
2019-02-10 18:01:06 +0100

    ça marche :D

--- ProjetTuteuré/Assets/Scripts/Character/RobotBossIA.cs
+++ ProjetTuteuré/Assets/Scripts/Character/RobotBossIA.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class RobotBossIA : MonoBehaviour
 {
     public int state,attackType;
-    public float attackTime,attack0Time;
+    public float attackTime,attack0Time,jumpTime,startPos;
 
     // Start is called before the first frame update
     void Start()
     {
-        state = 1;
-        GetComponent<Animator>().SetBool("hasFinishedJump", true);
-
+        state = 0;
+        jumpTime = Time.time;
+        startPos = gameObject.transform.position.y;
     }
 
     // Update is called once per frame
@@ -23,6 +23,19 @@ public class RobotBossIA : MonoBehaviour
             gameObject.GetComponent<Animator>().SetBool("hasDied", true);
             gameObject.GetComponent<EdgeCollider2D>().enabled = false;
             return;
+        }
+        if(state == 0)
+        {
+            if(Time.time - jumpTime > 1)
+            {
+                state = 1;
+                GetComponent<Animator>().SetBool("hasFinishedJump", true);
+                return;
+            } else
+            {
+                Vector3 pos = new Vector3(gameObject.transform.position.x, startPos + 3-(Mathf.Pow(1 - (Time.time-jumpTime)*2, 2)*3),0);
+                gameObject.transform.SetPositionAndRotation(pos, Quaternion.identity);
+            }
         }
         if(state == 1)
         {
